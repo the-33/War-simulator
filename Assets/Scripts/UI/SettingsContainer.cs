62547e4ac1c50b806f0517a87194397b2171ab85
Ikeer7 <alu.161068@usj.es>
2025-06-04 12:53:38 +0200

    Fixed errors in level

---
+++ Assets/Scripts/UI/SettingsContainer.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SettingsContainer : MonoBehaviour
+{
+    public float volume;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/UI/VolumeSlider.cs
+++ Assets/Scripts/UI/VolumeSlider.cs
@@ -6,18 +6,17 @@ public class VolumeSlider : MonoBehaviour
     public Slider m_VolumeSlider;
     public float m_VolumeSliderValue;
 
-    //public Slider m_SensititySlider;
-    public float m_SensititySliderValue = 100f;
-    //public Transform m_Playerbody;
-    private float xRotation = 0f;
+    //public SettingsContainer settings;
+
+   
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        m_SensititySliderValue = PlayerPrefs.GetFloat("CurrentSensitivity", 100);
-        m_VolumeSlider.value = PlayerPrefs.GetFloat("AudioVolume", 50f);
-        AudioListener.volume = m_VolumeSlider.value;
-        //m_SensititySlider.value = m_SensititySliderValue / 10;
+       
+        m_VolumeSlider.value = PlayerPrefs.GetFloat("AudioVolume", 100f);
+        //settings.volume = m_VolumeSlider.value;
+        
 
     }
 
@@ -26,22 +25,11 @@ public class VolumeSlider : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        PlayerPrefs.SetFloat("CurrentSensitivity", 100);
-        float mouseX = Input.GetAxis("Mouse X") * m_SensititySliderValue * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * m_SensititySliderValue * Time.deltaTime;
-
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        //m_Playerbody.Rotate(Vector3.up, mouseX);
+       
 
     }
 
-    public void AdjustSpeed (float speed)
-    {
-        m_SensititySliderValue = speed * 10;
-    }
+   
 
         
 
@@ -49,6 +37,6 @@ public class VolumeSlider : MonoBehaviour
     {
         m_VolumeSliderValue = value;
         m_VolumeSlider.value = PlayerPrefs.GetFloat("AudioVolume", m_VolumeSliderValue);
-        AudioListener.volume = m_VolumeSlider.value;
+        //settings.volume = m_VolumeSlider.value;
     }
 }
